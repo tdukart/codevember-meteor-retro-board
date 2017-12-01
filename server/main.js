61548647808ab44148eda7bde5eb7e743ee7fb7e
Todd Dukart <todd.dukart@globe.com>
2017-12-01 12:50:20 -0500

    Restrict logins to globe.com

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import '../imports/api/boards';
 import '../imports/api/stickies';
 
@@ -6,4 +7,11 @@ import './accountConfig';
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Accounts.validateNewUser((user) => {
+  if (user.services.google.email.match(/@globe\.com$/)) {
+    return true;
+  }
+  throw new Meteor.Error(403, 'You must sign in using a globe.com account');
 });
