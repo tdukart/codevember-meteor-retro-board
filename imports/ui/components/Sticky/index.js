615d6c47a6d3c7b450c8d9bc3916145e39b1be70
Todd Dukart <todd.dukart@globe.com>
2018-01-12 14:52:51 -0500

    Truncate and linkify long URLs (#28)

--- imports/ui/components/Sticky/index.js
+++ imports/ui/components/Sticky/index.js
@@ -1,8 +1,11 @@
+// eslint-ignore react/no-danger
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Glyphicon } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import emoji from 'node-emoji';
+import truncateUrl from 'truncate-url';
+import linkifyUrls from '@tdukart/linkify-urls';
 
 import CreateSticky from '../CreateSticky';
 
@@ -93,12 +96,18 @@ class Sticky extends React.Component {
       );
     }
 
+    const truncateUrlTo20 = url => (truncateUrl(url, 20));
+
+    let formattedBody = emoji.emojify(body);
+    formattedBody = linkifyUrls(formattedBody, { value: truncateUrlTo20, attributes: { target: '_blank' } });
+
+    let formattedNotes = emoji.emojify(notes);
+    formattedNotes = linkifyUrls(formattedNotes, { value: truncateUrlTo20, attributes: { target: '_blank' } });
+
     return (
       <div className={cssClasses.join(' ')}>
-        <p>{emoji.emojify(body)}</p>
-        <p className={styles.notes}>
-          {emoji.emojify(notes)}
-        </p>
+        <p dangerouslySetInnerHTML={{ __html: formattedBody }} />
+        <p className={styles.notes} dangerouslySetInnerHTML={{ __html: formattedNotes }} />
         <div className={styles.footer}>
           <Button onClick={startStickyEdit} bsSize="xsmall" bsStyle="default">
             <Glyphicon glyph="pencil" />
